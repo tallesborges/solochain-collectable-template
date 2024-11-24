ee6a82a138f88c0d3461b3d7e540f4f3bdc8ad7c
Talles Borges <talles.borges92@gmail.com>
2024-11-24 14:55:23 -0300

    feat: Add initial collectables pallet with basic storage and test functionality

---
+++ pallets/collectables/src/mock.rs
@@ -0,0 +1,31 @@
+use crate as pallet_collectables;
+use frame_support::derive_impl;
+use sp_runtime::BuildStorage;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+    pub enum Test
+    {
+        System: frame_system,
+        CollectablesModule: pallet_collectables,
+    }
+);
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+    type Block = Block;
+}
+
+impl pallet_collectables::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
+        .unwrap()
+        .into()
+}
